ad78efce9008963e3c57b65d795fdfd2ca7458c1
Miguel Czyz <mc@cs3b.com>
2026-01-12 01:24:48 +0000

    feat(ace-support-timestamp): rename gem from ace-timestamp to ace-support-timestamp
    - Rename gem to ace-support-timestamp for naming consistency with other ace-support-* gems
    - Update namespace from Ace::Timestamp to Ace::Support::Timestamp
    - Update executable name to ace-support-timestamp
    - Update all requires and references throughout codebase

---
+++ ace-support-timestamp/lib/ace/support/timestamp/version.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Ace
+  module Support
+    module Timestamp
+      # Version 0.2.0: Gem renamed from ace-timestamp to ace-support-timestamp
+      # Namespace changed from Ace::Timestamp to Ace::Support::Timestamp
+      VERSION = "0.2.0"
+    end
+  end
+end
