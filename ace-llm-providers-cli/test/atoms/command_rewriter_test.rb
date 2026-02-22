28cdafab1635fb05f5676e4fb92e9d6643724227
Michal Czyz <miguel@cs3b.com>
2026-02-22 00:19:07 +0000

    chore(release): patch all modified packages for hyphenated skill naming

--- ace-llm-providers-cli/test/atoms/command_rewriter_test.rb
+++ ace-llm-providers-cli/test/atoms/command_rewriter_test.rb
@@ -10,50 +10,50 @@ module Ace
       module CLI
         module Atoms
           class CommandRewriterTest < Minitest::Test
-            SKILLS = %w[ace_onboard ace_git_commit ace_review_pr commit search].freeze
+            SKILLS = %w[ace-onboard ace-git-commit ace-review-pr commit search].freeze
 
             # PI Formatter Tests
             def test_pi_formatter_rewrites_simple_skill_at_start_of_line
-              result = CommandRewriter.call("/ace_onboard", skill_names: SKILLS, formatter: CommandFormatters::PI_FORMATTER)
-              assert_equal "/skill:ace_onboard", result
+              result = CommandRewriter.call("/ace-onboard", skill_names: SKILLS, formatter: CommandFormatters::PI_FORMATTER)
+              assert_equal "/skill:ace-onboard", result
             end
 
             def test_pi_formatter_rewrites_skill_after_whitespace
-              result = CommandRewriter.call("Please run /ace_onboard first", skill_names: SKILLS, formatter: CommandFormatters::PI_FORMATTER)
-              assert_equal "Please run /skill:ace_onboard first", result
+              result = CommandRewriter.call("Please run /ace-onboard first", skill_names: SKILLS, formatter: CommandFormatters::PI_FORMATTER)
+              assert_equal "Please run /skill:ace-onboard first", result
             end
 
             def test_pi_formatter_rewrites_underscore_skill_names
-              result = CommandRewriter.call("/ace_git_commit", skill_names: SKILLS, formatter: CommandFormatters::PI_FORMATTER)
-              assert_equal "/skill:ace_git_commit", result
+              result = CommandRewriter.call("/ace-git-commit", skill_names: SKILLS, formatter: CommandFormatters::PI_FORMATTER)
+              assert_equal "/skill:ace-git-commit", result
             end
 
             def test_pi_formatter_rewrites_multiple_skills_in_same_prompt
-              input = "Run /ace_onboard then /ace_git_commit"
+              input = "Run /ace-onboard then /ace-git-commit"
               result = CommandRewriter.call(input, skill_names: SKILLS, formatter: CommandFormatters::PI_FORMATTER)
-              assert_equal "Run /skill:ace_onboard then /skill:ace_git_commit", result
+              assert_equal "Run /skill:ace-onboard then /skill:ace-git-commit", result
             end
 
             # Codex Formatter Tests
             def test_codex_formatter_rewrites_simple_skill_at_start_of_line
-              result = CommandRewriter.call("/ace_onboard", skill_names: SKILLS, formatter: CommandFormatters::CODEX_FORMATTER)
-              assert_equal "$ace_onboard", result
+              result = CommandRewriter.call("/ace-onboard", skill_names: SKILLS, formatter: CommandFormatters::CODEX_FORMATTER)
+              assert_equal "$ace-onboard", result
             end
 
             def test_codex_formatter_rewrites_skill_after_whitespace
-              result = CommandRewriter.call("Please run /ace_onboard first", skill_names: SKILLS, formatter: CommandFormatters::CODEX_FORMATTER)
-              assert_equal "Please run $ace_onboard first", result
+              result = CommandRewriter.call("Please run /ace-onboard first", skill_names: SKILLS, formatter: CommandFormatters::CODEX_FORMATTER)
+              assert_equal "Please run $ace-onboard first", result
             end
 
             def test_codex_formatter_preserves_underscores
-              result = CommandRewriter.call("/ace_git_commit", skill_names: SKILLS, formatter: CommandFormatters::CODEX_FORMATTER)
-              assert_equal "$ace_git_commit", result
+              result = CommandRewriter.call("/ace-git-commit", skill_names: SKILLS, formatter: CommandFormatters::CODEX_FORMATTER)
+              assert_equal "$ace-git-commit", result
             end
 
             def test_codex_formatter_rewrites_multiple_skills_in_same_prompt
-              input = "Run /ace_onboard then /ace_git_commit"
+              input = "Run /ace-onboard then /ace-git-commit"
               result = CommandRewriter.call(input, skill_names: SKILLS, formatter: CommandFormatters::CODEX_FORMATTER)
-              assert_equal "Run $ace_onboard then $ace_git_commit", result
+              assert_equal "Run $ace-onboard then $ace-git-commit", result
             end
 
             # Common behavior tests (applies to both formatters)
@@ -68,7 +68,7 @@ module Ace
               input = <<~TEXT
                 Run this:
                 ```
-                /ace_onboard
+                /ace-onboard
                 ```
                 Then /commit
               TEXT
@@ -76,9 +76,9 @@ module Ace
               pi_result = CommandRewriter.call(input, skill_names: SKILLS, formatter: CommandFormatters::PI_FORMATTER)
               codex_result = CommandRewriter.call(input, skill_names: SKILLS, formatter: CommandFormatters::CODEX_FORMATTER)
 
-              # Both should preserve /ace_onboard inside code block
-              assert_includes pi_result, "/ace_onboard"
-              assert_includes codex_result, "/ace_onboard"
+              # Both should preserve /ace-onboard inside code block
+              assert_includes pi_result, "/ace-onboard"
+              assert_includes codex_result, "/ace-onboard"
 
               # Both should rewrite /commit outside code block
               assert_includes pi_result, "/skill:commit"
@@ -86,14 +86,14 @@ module Ace
             end
 
             def test_skips_inline_code_spans
-              input = "Use `/ace_onboard` to start, then run /commit"
+              input = "Use `/ace-onboard` to start, then run /commit"
 
               pi_result = CommandRewriter.call(input, skill_names: SKILLS, formatter: CommandFormatters::PI_FORMATTER)
               codex_result = CommandRewriter.call(input, skill_names: SKILLS, formatter: CommandFormatters::CODEX_FORMATTER)
 
-              # Both should preserve `/ace_onboard` inside backticks
-              assert_includes pi_result, "`/ace_onboard`"
-              assert_includes codex_result, "`/ace_onboard`"
+              # Both should preserve `/ace-onboard` inside backticks
+              assert_includes pi_result, "`/ace-onboard`"
+              assert_includes codex_result, "`/ace-onboard`"
 
               # Both should rewrite /commit outside backticks
               assert_includes pi_result, "/skill:commit"
@@ -101,24 +101,24 @@ module Ace
             end
 
             def test_does_not_rewrite_urls
-              input = "Visit https://example.com/ace_onboard for details"
+              input = "Visit https://example.com/ace-onboard for details"
 
               pi_result = CommandRewriter.call(input, skill_names: SKILLS, formatter: CommandFormatters::PI_FORMATTER)
               codex_result = CommandRewriter.call(input, skill_names: SKILLS, formatter: CommandFormatters::CODEX_FORMATTER)
 
-              assert_equal "Visit https://example.com/ace_onboard for details", pi_result
-              assert_equal "Visit https://example.com/ace_onboard for details", codex_result
+              assert_equal "Visit https://example.com/ace-onboard for details", pi_result
+              assert_equal "Visit https://example.com/ace-onboard for details", codex_result
             end
 
             def test_longest_match_first
-              skills = %w[ace_review_pr ace_review]
-              input = "/ace_review_pr please"
+              skills = %w[ace-review-pr ace_review]
+              input = "/ace-review-pr please"
 
               pi_result = CommandRewriter.call(input, skill_names: skills, formatter: CommandFormatters::PI_FORMATTER)
               codex_result = CommandRewriter.call(input, skill_names: skills, formatter: CommandFormatters::CODEX_FORMATTER)
 
-              assert_equal "/skill:ace_review_pr please", pi_result
-              assert_equal "$ace_review_pr please", codex_result
+              assert_equal "/skill:ace-review-pr please", pi_result
+              assert_equal "$ace-review-pr please", codex_result
             end
 
             def test_returns_original_for_nil_prompt
@@ -138,51 +138,51 @@ module Ace
             end
 
             def test_returns_original_for_nil_skill_names
-              pi_result = CommandRewriter.call("/ace_onboard", skill_names: nil, formatter: CommandFormatters::PI_FORMATTER)
-              codex_result = CommandRewriter.call("/ace_onboard", skill_names: nil, formatter: CommandFormatters::CODEX_FORMATTER)
+              pi_result = CommandRewriter.call("/ace-onboard", skill_names: nil, formatter: CommandFormatters::PI_FORMATTER)
+              codex_result = CommandRewriter.call("/ace-onboard", skill_names: nil, formatter: CommandFormatters::CODEX_FORMATTER)
 
-              assert_equal "/ace_onboard", pi_result
-              assert_equal "/ace_onboard", codex_result
+              assert_equal "/ace-onboard", pi_result
+              assert_equal "/ace-onboard", codex_result
             end
 
             def test_returns_original_for_empty_skill_names
-              pi_result = CommandRewriter.call("/ace_onboard", skill_names: [], formatter: CommandFormatters::PI_FORMATTER)
-              codex_result = CommandRewriter.call("/ace_onboard", skill_names: [], formatter: CommandFormatters::CODEX_FORMATTER)
+              pi_result = CommandRewriter.call("/ace-onboard", skill_names: [], formatter: CommandFormatters::PI_FORMATTER)
+              codex_result = CommandRewriter.call("/ace-onboard", skill_names: [], formatter: CommandFormatters::CODEX_FORMATTER)
 
-              assert_equal "/ace_onboard", pi_result
-              assert_equal "/ace_onboard", codex_result
+              assert_equal "/ace-onboard", pi_result
+              assert_equal "/ace-onboard", codex_result
             end
 
             def test_skill_at_end_of_line
-              pi_result = CommandRewriter.call("Run /ace_onboard", skill_names: SKILLS, formatter: CommandFormatters::PI_FORMATTER)
-              codex_result = CommandRewriter.call("Run /ace_onboard", skill_names: SKILLS, formatter: CommandFormatters::CODEX_FORMATTER)
+              pi_result = CommandRewriter.call("Run /ace-onboard", skill_names: SKILLS, formatter: CommandFormatters::PI_FORMATTER)
+              codex_result = CommandRewriter.call("Run /ace-onboard", skill_names: SKILLS, formatter: CommandFormatters::CODEX_FORMATTER)
 
-              assert_equal "Run /skill:ace_onboard", pi_result
-              assert_equal "Run $ace_onboard", codex_result
+              assert_equal "Run /skill:ace-onboard", pi_result
+              assert_equal "Run $ace-onboard", codex_result
             end
 
             def test_multiline_prompt
-              input = "/ace_onboard\nThen /ace_git_commit\nDone"
+              input = "/ace-onboard\nThen /ace-git-commit\nDone"
 
               pi_result = CommandRewriter.call(input, skill_names: SKILLS, formatter: CommandFormatters::PI_FORMATTER)
               codex_result = CommandRewriter.call(input, skill_names: SKILLS, formatter: CommandFormatters::CODEX_FORMATTER)
 
-              assert_includes pi_result, "/skill:ace_onboard"
-              assert_includes pi_result, "/skill:ace_git_commit"
-              assert_includes codex_result, "$ace_onboard"
-              assert_includes codex_result, "$ace_git_commit"
+              assert_includes pi_result, "/skill:ace-onboard"
+              assert_includes pi_result, "/skill:ace-git-commit"
+              assert_includes codex_result, "$ace-onboard"
+              assert_includes codex_result, "$ace-git-commit"
             end
 
             def test_does_not_rewrite_file_paths
-              input = "Check /usr/bin/ace_onboard"
+              input = "Check /usr/bin/ace-onboard"
 
               pi_result = CommandRewriter.call(input, skill_names: SKILLS, formatter: CommandFormatters::PI_FORMATTER)
               codex_result = CommandRewriter.call(input, skill_names: SKILLS, formatter: CommandFormatters::CODEX_FORMATTER)
 
-              # "/usr/bin/ace_onboard" — the "/ace_onboard" part is preceded by "/bin"
+              # "/usr/bin/ace-onboard" — the "/ace-onboard" part is preceded by "/bin"
               # so it should not match (not preceded by whitespace)
-              refute_includes pi_result, "/skill:ace_onboard"
-              refute_includes codex_result, "$ace_onboard"
+              refute_includes pi_result, "/skill:ace-onboard"
+              refute_includes codex_result, "$ace-onboard"
             end
           end
         end
--- ace-llm-providers-cli/test/atoms/skill_command_rewriter_test.rb
+++ ace-llm-providers-cli/test/atoms/skill_command_rewriter_test.rb
@@ -9,27 +9,27 @@ module Ace
       module CLI
         module Atoms
           class SkillCommandRewriterTest < Minitest::Test
-            SKILLS = %w[ace_onboard ace_git_commit ace_review_pr commit search].freeze
+            SKILLS = %w[ace-onboard ace-git-commit ace-review-pr commit search].freeze
 
             def test_rewrites_simple_skill_at_start_of_line
-              result = SkillCommandRewriter.call("/ace_onboard", skill_names: SKILLS)
-              assert_equal "/skill:ace_onboard", result
+              result = SkillCommandRewriter.call("/ace-onboard", skill_names: SKILLS)
+              assert_equal "/skill:ace-onboard", result
             end
 
             def test_rewrites_skill_after_whitespace
-              result = SkillCommandRewriter.call("Please run /ace_onboard first", skill_names: SKILLS)
-              assert_equal "Please run /skill:ace_onboard first", result
+              result = SkillCommandRewriter.call("Please run /ace-onboard first", skill_names: SKILLS)
+              assert_equal "Please run /skill:ace-onboard first", result
             end
 
             def test_rewrites_underscore_skill_names
-              result = SkillCommandRewriter.call("/ace_git_commit", skill_names: SKILLS)
-              assert_equal "/skill:ace_git_commit", result
+              result = SkillCommandRewriter.call("/ace-git-commit", skill_names: SKILLS)
+              assert_equal "/skill:ace-git-commit", result
             end
 
             def test_rewrites_multiple_skills_in_same_prompt
-              input = "Run /ace_onboard then /ace_git_commit"
+              input = "Run /ace-onboard then /ace-git-commit"
               result = SkillCommandRewriter.call(input, skill_names: SKILLS)
-              assert_equal "Run /skill:ace_onboard then /skill:ace_git_commit", result
+              assert_equal "Run /skill:ace-onboard then /skill:ace-git-commit", result
             end
 
             def test_does_not_rewrite_unknown_names
@@ -39,44 +39,44 @@ module Ace
 
             def test_does_not_double_rewrite_skill_prefix
               # If prompt already has /skill:name, it should NOT become /skill:skill:name
-              result = SkillCommandRewriter.call("/skill:ace_onboard", skill_names: SKILLS)
-              assert_equal "/skill:ace_onboard", result
+              result = SkillCommandRewriter.call("/skill:ace-onboard", skill_names: SKILLS)
+              assert_equal "/skill:ace-onboard", result
             end
 
             def test_skips_fenced_code_blocks
               input = <<~TEXT
                 Run this:
                 ```
-                /ace_onboard
+                /ace-onboard
                 ```
                 Then /commit
               TEXT
               result = SkillCommandRewriter.call(input, skill_names: SKILLS)
-              assert_includes result, "/ace_onboard"       # preserved inside code block
+              assert_includes result, "/ace-onboard"       # preserved inside code block
               assert_includes result, "/skill:commit"  # rewritten outside code block
-              refute_includes result, "/skill:ace_onboard" # NOT rewritten inside code block
+              refute_includes result, "/skill:ace-onboard" # NOT rewritten inside code block
             end
 
             def test_skips_inline_code_spans
-              input = "Use `/ace_onboard` to start, then run /commit"
+              input = "Use `/ace-onboard` to start, then run /commit"
               result = SkillCommandRewriter.call(input, skill_names: SKILLS)
-              assert_includes result, "`/ace_onboard`"      # preserved inside backticks
+              assert_includes result, "`/ace-onboard`"      # preserved inside backticks
               assert_includes result, "/skill:commit"    # rewritten outside backticks
             end
 
             def test_does_not_rewrite_urls
               # URLs have slash preceded by another slash or domain chars — not whitespace
-              input = "Visit https://example.com/ace_onboard for details"
+              input = "Visit https://example.com/ace-onboard for details"
               result = SkillCommandRewriter.call(input, skill_names: SKILLS)
-              assert_equal "Visit https://example.com/ace_onboard for details", result
+              assert_equal "Visit https://example.com/ace-onboard for details", result
             end
 
             def test_longest_match_first
-              # "ace_review_pr" should match before "ace_review" in partial overlaps
-              skills = %w[ace_review_pr ace_review]
-              input = "/ace_review_pr please"
+              # "ace-review-pr" should match before "ace_review" in partial overlaps
+              skills = %w[ace-review-pr ace_review]
+              input = "/ace-review-pr please"
               result = SkillCommandRewriter.call(input, skill_names: skills)
-              assert_equal "/skill:ace_review_pr please", result
+              assert_equal "/skill:ace-review-pr please", result
             end
 
             def test_returns_original_for_nil_prompt
@@ -90,34 +90,34 @@ module Ace
             end
 
             def test_returns_original_for_nil_skill_names
-              result = SkillCommandRewriter.call("/ace_onboard", skill_names: nil)
-              assert_equal "/ace_onboard", result
+              result = SkillCommandRewriter.call("/ace-onboard", skill_names: nil)
+              assert_equal "/ace-onboard", result
             end
 
             def test_returns_original_for_empty_skill_names
-              result = SkillCommandRewriter.call("/ace_onboard", skill_names: [])
-              assert_equal "/ace_onboard", result
+              result = SkillCommandRewriter.call("/ace-onboard", skill_names: [])
+              assert_equal "/ace-onboard", result
             end
 
             def test_skill_at_end_of_line
-              result = SkillCommandRewriter.call("Run /ace_onboard", skill_names: SKILLS)
-              assert_equal "Run /skill:ace_onboard", result
+              result = SkillCommandRewriter.call("Run /ace-onboard", skill_names: SKILLS)
+              assert_equal "Run /skill:ace-onboard", result
             end
 
             def test_multiline_prompt
-              input = "/ace_onboard\nThen /ace_git_commit\nDone"
+              input = "/ace-onboard\nThen /ace-git-commit\nDone"
               result = SkillCommandRewriter.call(input, skill_names: SKILLS)
-              assert_includes result, "/skill:ace_onboard"
-              assert_includes result, "/skill:ace_git_commit"
+              assert_includes result, "/skill:ace-onboard"
+              assert_includes result, "/skill:ace-git-commit"
             end
 
             def test_does_not_rewrite_file_paths
-              # File paths like /usr/bin/ace_onboard have slash before the name
-              input = "Check /usr/bin/ace_onboard"
+              # File paths like /usr/bin/ace-onboard have slash before the name
+              input = "Check /usr/bin/ace-onboard"
               result = SkillCommandRewriter.call(input, skill_names: SKILLS)
-              # "/usr/bin/ace_onboard" — the "/ace_onboard" part is preceded by "/bin"
+              # "/usr/bin/ace-onboard" — the "/ace-onboard" part is preceded by "/bin"
               # so it should not match (not preceded by whitespace)
-              refute_includes result, "/skill:ace_onboard"
+              refute_includes result, "/skill:ace-onboard"
             end
           end
         end
