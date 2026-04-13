18e4a6ff05d4ea37cf8f06f8a13cf96f5a8cd9f4
Michal Czyz <miguel@cs3b.com>
2026-04-13 10:42:53 +0100

    feat(ace-git-commit): migrate to fast/feat/e2e test model
    - Move deterministic suites to test/fast/.
    - Update scenario metadata and testing contract documentation.

--- ace-git-commit/lib/ace/git_commit/version.rb
+++ ace-git-commit/lib/ace/git_commit/version.rb
@@ -2,6 +2,6 @@
 
 module Ace
   module GitCommit
-    VERSION = '0.23.7'
+    VERSION = '0.24.1'
   end
 end
--- ace-git-commit/test/fast/models/split_commit_result_test.rb
+++ ace-git-commit/test/fast/models/split_commit_result_test.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative "../test_helper"
+require_relative "../../test_helper"
 
 class SplitCommitResultTest < TestCase
   def test_tracks_success_and_failure
